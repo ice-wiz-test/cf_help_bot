1cf17072688332c72b2561be2027f30dd9cab659
Cheaplifer <larichev.po@students.sch2.ru>
2023-07-09 22:52:27 +0300

    Simple code for adding in the connection string and making the connection to the database

--- main.go
+++ main.go
@@ -3,9 +3,27 @@ package main
 import (
 	bot "cf_help_bot/bot"
 	web "cf_help_bot/web"
+	"database/sql"
+	_ "github.com/lib/pq"
+	"os"
 )
 
+func get_connection_string() string {
+	filePath := "connection_string.txt"
+	readFile, err := os.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+	return string(readFile)
+}
+
 func main() {
+	connstr := get_connection_string()
+	db, err := sql.Open("postgres", connstr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 	go bot.Initialize()
 	go web.Start()
 	select {}
