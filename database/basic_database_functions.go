1010f66a3763d98d58ef01f02d045562dce2f6c5
LeftTry <lerest.go@gmail.com>
2023-07-12 00:02:01 +0600

    Added DataBase methods

--- database/basic_database_functions.go
+++ database/basic_database_functions.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	user "cf_help_bot/user"
 	"context"
 	"log"
 
@@ -14,3 +15,47 @@ func get_connection() (error, *db.Conn) {
 	}
 	return err, conn
 }
+
+func Get_user_data(conn *db.Conn, userId int64) ([]interface{}, error) {
+	rows, err := conn.Query(context.Background(), "select * from users where userID=$1", userId)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var data []interface{}
+	if rows.Next() {
+		data, err = rows.Values()
+		if err == nil {
+			log.Fatal(err)
+		}
+	}
+	return data, nil
+}
+
+func Set_user_data(conn *db.Conn, userId int64, u user.User, isLangSelected bool, isLangSelection bool, lang string) {
+	var lang_db int
+	if lang == "eng" {
+		lang_db = 0
+	} else if lang == "rus" {
+		lang_db = 1
+	}
+	_, err := conn.Exec(context.Background(), "insert into users(handle, isSettingLocalization, hasSetLocalization, localization, userID) values ($1, $2, $3, $4, $5)", u.GetHandle(), isLangSelection, isLangSelected, lang_db, userId)
+	if err == nil {
+		log.Fatal(err)
+	}
+}
+
+// TODO
+func Update_user_data(conn *db.Conn, userId int64) {
+
+}
+
+// TODO
+func Delete_user_data(conn *db.Conn, userId int64) {
+
+}
+
+// TODO
+func Update_problems_data(conn *db.Conn) {}
+
+// TODO one time use func
+func get_problems_data(conn *db.Conn) {}
--- main.go
+++ main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	bot "cf_help_bot/bot"
-	//database "cf_help_bot/database"
+	database "cf_help_bot/database"
 	web "cf_help_bot/web"
-	//"database/sql"
+	"database/sql"
 
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	//connstr := database.Get_connection_string()
-	//db, err := sql.Open("postgres", connstr)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//defer db.Close()
+	connstr := database.Get_connection_string()
+	db, err := sql.Open("postgres", connstr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 	go bot.Initialize()
 	go web.Start()
 	select {}
