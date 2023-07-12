d8a755de47d8982b437d0b6eaaad5bda2322092c
Cheaplifer <larichev.po@students.sch2.ru>
2023-07-12 14:54:11 +0300

    Very small changes

--- main.go
+++ main.go
@@ -2,20 +2,11 @@ package main
 
 import (
 	bot "cf_help_bot/bot"
-	database "cf_help_bot/database"
 	web "cf_help_bot/web"
-	"database/sql"
-
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	connstr := database.Get_connection_string()
-	db, err := sql.Open("postgres", connstr)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
 	go bot.Initialize()
 	go web.Start()
 	select {}
